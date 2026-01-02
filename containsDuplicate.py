198f48ebed0825258e8d916cb1455feb0048b3b9
RafiuddinAhmedAkhlaque <rafiuddinakhlaque@gmail.com>
2026-01-01 23:53:21 -0700

    Second leetcode problem edit

--- containsDuplicate.py
+++ containsDuplicate.py
@@ -12,3 +12,16 @@ class Solution:
     
 instance = Solution()
 print(instance.containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
+
+class Solution2:
+    def test(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            else:
+                seen.add(num)
+        return False
+    
+sol = Solution2()
+print(Solution2().test([1,2,3]))
